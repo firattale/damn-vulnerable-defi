9acef65120bf99033b004d612a07bc1323d78f84
firatstory.eth <talefirat@gmail.com>
2025-07-11 14:38:07 +0200

    4- solve SideEntrance

--- test/side-entrance/SideEntrance.t.sol
+++ test/side-entrance/SideEntrance.t.sol
@@ -4,6 +4,7 @@ pragma solidity =0.8.25;
 
 import {Test, console} from "forge-std/Test.sol";
 import {SideEntranceLenderPool} from "../../src/side-entrance/SideEntranceLenderPool.sol";
+import {SideEntranceLenderPoolExploiter} from "../../src/side-entrance/SideEntranceLenderPoolExploiter.sol";
 
 contract SideEntranceChallenge is Test {
     address deployer = makeAddr("deployer");
@@ -45,7 +46,11 @@ contract SideEntranceChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_sideEntrance() public checkSolvedByPlayer {
-        
+        SideEntranceLenderPoolExploiter exploiter = new SideEntranceLenderPoolExploiter(
+                address(pool)
+            );
+
+        exploiter.exploit(recovery);
     }
 
     /**
@@ -53,6 +58,10 @@ contract SideEntranceChallenge is Test {
      */
     function _isSolved() private view {
         assertEq(address(pool).balance, 0, "Pool still has ETH");
-        assertEq(recovery.balance, ETHER_IN_POOL, "Not enough ETH in recovery account");
+        assertEq(
+            recovery.balance,
+            ETHER_IN_POOL,
+            "Not enough ETH in recovery account"
+        );
     }
 }
